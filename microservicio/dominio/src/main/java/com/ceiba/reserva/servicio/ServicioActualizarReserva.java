3b7b1bbf4c4250eb6804fba88a85ab454d19eb0c
Carlos Javier Suarez Gonzalez - Ceiba Software <javier.suarez@ceiba.com.co>
2022-02-22 07:55:51 -0500

    Se agregan archivos de integración continua

--- microservicio/dominio/src/main/java/com/ceiba/reserva/servicio/ServicioActualizarReserva.java
+++ microservicio/dominio/src/main/java/com/ceiba/reserva/servicio/ServicioActualizarReserva.java
@@ -4,7 +4,6 @@ import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
 import com.ceiba.dominio.excepcion.ExcepcionValorInvalido;
 import com.ceiba.reserva.modelo.entidad.Reserva;
 import com.ceiba.reserva.puerto.repositorio.RepositorioReserva;
-import com.ceiba.reserva.util.Calculo;
 
 import java.time.DayOfWeek;
 
@@ -18,7 +17,6 @@ public class ServicioActualizarReserva {
     private static final int CIEN = 100;
     private static final Long TARIFA_FIJA = Long.valueOf(200000);
     private final RepositorioReserva repositorioReserva;
-    private final Calculo calculo;
 
     public ServicioActualizarReserva(RepositorioReserva respositorioReserva) {
         this.repositorioReserva = respositorioReserva;
@@ -43,8 +41,7 @@ public class ServicioActualizarReserva {
             throw new ExcepcionValorInvalido(LA_FECHA_DE_RESERVA_NO_ES_VALIDA);
         }
     }
-
-    public Long calcularCostoReserva(Reserva reserva){
+   public Long calcularCostoReserva(Reserva reserva){
         Long costoReserva = TARIFA_FIJA +  calcularPorcentaje(VEINTE);
         if(reserva.getFechaReserva().getDayOfWeek() == DayOfWeek.SATURDAY){
             costoReserva =  costoReserva + calcularPorcentaje(DIEZ);
